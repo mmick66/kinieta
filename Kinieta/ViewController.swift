65a6c94f8402bb9d6180065b21deea1babc7ed83
Michael Michailidis <mmick66@gmail.com>
2017-08-31 08:02:58 +0200

    Initial Commit

---
+++ Kinieta/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kinieta
+//
+//  Created by Michael Michailidis on 31/08/2017.
+//  Copyright © 2017 Michael Michailidis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ KinietaTests/KinietaTests.swift
@@ -0,0 +1,36 @@
+//
+//  KinietaTests.swift
+//  KinietaTests
+//
+//  Created by Michael Michailidis on 31/08/2017.
+//  Copyright © 2017 Michael Michailidis. All rights reserved.
+//
+
+import XCTest
+@testable import Kinieta
+
+class KinietaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
