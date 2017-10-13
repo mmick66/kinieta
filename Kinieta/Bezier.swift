475d68625a018c3df56f8fa767f82b5b3e5a84f0
Michael Michailidis <mmichailidis@theworkshop.com>
2017-10-13 11:32:06 +0200

    Refactored Bezier code introducing precalculated tables to multiply the speed of resolution

--- Kinieta/Bezier.swift
+++ Kinieta/Bezier.swift
@@ -11,16 +11,46 @@ import UIKit
 
 struct Bezier {
     
-    struct Point {
+    static var Accuracy = 1000
+    static let Factors  = Table(with: Bezier.Accuracy)
+    
+    struct Table {
+        let C0:[Double]
+        let C1:[Double]
+        let C2:[Double]
+        let C3:[Double]
+        init(with steps: Int) {
+            var _C0 = [Double](repeating: 0.0, count: steps+1)
+            var _C1 = [Double](repeating: 0.0, count: steps+1)
+            var _C2 = [Double](repeating: 0.0, count: steps+1)
+            var _C3 = [Double](repeating: 0.0, count: steps+1)
+            for step in 0...steps {
+                let T = Double(step)/Double(steps);
+                _C0[step] = (1.0 - T) * (1.0 - T) * (1.0 - T);
+                _C1[step] = 3.0 * (1.0 - T) * (1.0 - T) * T;
+                _C2[step] = 3.0 * (1.0 - T) * T * T;
+                _C3[step] = T * T * T;
+            }
+            C0 = _C0
+            C1 = _C1
+            C2 = _C2
+            C3 = _C3
+        }
+    }
+    
+    struct Point: CustomStringConvertible {
         
         let x: Double
         let y: Double
-        init(_ x: Double, _ y: Double) {
+        init(_ x: Double = 0.0, _ y: Double = 0.0) {
             self.x = x
             self.y = y
         }
         var cgPoint: CGPoint {
             return CGPoint(x: self.x, y: self.y)
+        }
+        static func *(lhs:Point, rhs:Double) -> Point {
+            return Point(lhs.x * rhs, lhs.y * rhs)
         }
         static func *(lhs:Double, rhs:Point) -> Point {
             return Point(lhs * rhs.x, lhs * rhs.y)
@@ -28,28 +58,32 @@ struct Bezier {
         static func +(lhs:Point, rhs:Point) -> Point {
             return Point(lhs.x + rhs.x, lhs.y + rhs.y)
         }
+        var description: String {
+            return "(x:\(self.x), y:\(self.y))"
+        }
     }
     
-    let p0:Point = Point(0.0, 0.0)
-    let p1:Point
-    let p2:Point
-    let p3:Point = Point(1.0, 1.0)
+    let P0:Point = Point(0.0, 0.0)
+    let P1:Point
+    let P2:Point
+    let P3:Point = Point(1.0, 1.0)
     
+    let POINTS:[Point]
     init(_ p1x:Double, _ p1y:Double, _ p2x:Double, _ p2y:Double) {
-        p1 = Point(p1x, p1y)
-        p2 = Point(p2x, p2y)
+        P1 = Point(p1x, p1y)
+        P2 = Point(p2x, p2y)
+        let F = Bezier.Factors
+        var _P = [Point](repeating: Point(), count: Bezier.Accuracy+1)
+        for step in 0...Bezier.Accuracy {
+            _P[step] = F.C0[step] * P0 + F.C1[step] * P1 + F.C2[step] * P2 + F.C3[step] * P3
+        }
+        POINTS = _P
     }
+    
     
     func solve(_ t:Double) -> Double {
-        
-        let omt = 1.0 - t
-        let tsq = t * t
-        
-        let tm1 = (3 * t * omt * omt) * p1
-        let tm2 = (3 * tsq * omt) * p2
-        let tm3 = (t * tsq) * p3
-        
-        return (tm1 + tm2 + tm3).y
+        let T = Int(t * Double(Bezier.Accuracy))
+        return POINTS[T].y
     }
 }
 
--- Kinieta/ViewController.swift
+++ Kinieta/ViewController.swift
@@ -24,6 +24,7 @@ class ViewController: UIViewController {
     @IBAction func goButtonPressed(_ sender: UIButton) {
         self.square
             .move(to: ["x": 250, "y": 500, "r": 10], during: 1.0).easeInOut()
+            .wait(for: 0.5)
             .move(to: ["x": 150, "y": 200, "r": 30], during: 1.0).easeInOut()
         
     }
